019efbb1f21fd7db7c31becccc69161e3664a2b0
Santiago Faci <santiago.faci@gmail.com>
2017-11-04 02:50:36 +0100

    new views and rules to add and delete items

--- ABC/urls.py
+++ ABC/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'items/$', views.items, name='items'),
-    url(r'signin/$', views.signin, name='signin')
+    url(r'add_item/$', views.add_item, name='add_item'),
+    url(r'^remove_item/(?P<item_id>[0-9]*)/$', views.remove_item, name='remove_item'),
+    url(r'^item/(?P<item_id>[0-9]*)/$', views.item, name='item'),
+    url(r'signin/$', views.signin, name='signin'),
+    url(r'tasks/$', views.tasks, name='tasks')
 ]
--- ABC/views.py
+++ ABC/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template import loader
+from django.urls import reverse
+from ABC.models import Item
 
 
 def index(request):
@@ -10,7 +11,34 @@ def index(request):
 
 
 def items(request):
+    all_items = Item.objects.order_by('name')
+
     template = loader.get_template('ABC/items.html')
+    context = {'items': all_items,}
+    return HttpResponse(template.render(context, request))
+
+
+def item(request, item_id):
+    template = loader.get_template('ABC/item.html')
+    the_item = Item.objects.get(id=item_id)
+    context = {'the_item':the_item}
+    return HttpResponse(template.render(context, request))
+
+
+def add_item(request):
+    an_item = Item.create(request.GET['name'], request.GET['description'], request.GET['price'])
+    an_item.save()
+    return HttpResponseRedirect(reverse('items'))
+
+
+def remove_item(request, item_id):
+    an_item = Item.objects.get(id=item_id)
+    an_item.delete()
+    return HttpResponseRedirect(reverse('items'))
+
+
+def tasks(request):
+    template = loader.get_template('ABC/tasks.html')
     context = {}
     return HttpResponse(template.render(context, request))
 
